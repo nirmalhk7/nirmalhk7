599bc1677b8d8aa51aad3e1ba17700702cbbc91f
181IT230 <nirmalhk7@gmail.com>
2019-06-03 12:50:42 +0300

    Daily Schedule Activity added

--- app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
@@ -42,6 +42,9 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        Intent i = new Intent(MainActivity.this, DailySchedule.class);
+        startActivity(i);
+
     }
 
     @Override
