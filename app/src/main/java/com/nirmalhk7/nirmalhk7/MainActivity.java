929d2ff939faa4b9787f0fdc5ead8ba7aa864dfc
181IT230 <nirmalhk7@gmail.com>
2019-06-20 15:37:37 +0300

    Working spl screen

--- app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         if (savedInstanceState == null) {
             navigationView.getMenu().performIdentifierAction(R.id.nav_dashboard, 0);
