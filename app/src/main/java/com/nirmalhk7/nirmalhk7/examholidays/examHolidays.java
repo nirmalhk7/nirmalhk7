2b70888db643dc79f3355b158a18bd4e7628e459
181IT230 <nirmalhk7@gmail.com>
2019-07-03 16:23:29 +0300

    List readied

---
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/examholidays/examHolidays.java
@@ -0,0 +1,129 @@
+package com.nirmalhk7.nirmalhk7.examholidays;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.nirmalhk7.nirmalhk7.R;
+import com.nirmalhk7.nirmalhk7.dailyscheduler.ScheduleAdapter;
+import com.nirmalhk7.nirmalhk7.dailyscheduler.scheduleItem;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link examHolidays.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link examHolidays#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class examHolidays extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public examHolidays() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment examHolidays.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static examHolidays newInstance(String param1, String param2) {
+        examHolidays fragment = new examHolidays();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView= inflater.inflate(R.layout.fragment_exam_holidays, container, false);
+        ArrayList<hsItem> hs = new ArrayList<hsItem>();
+        hs.add(new hsItem(1,"Makar Sankrant","3 May"));
+        hs.add(new hsItem(0,"Mid Sem Exam","3 May"));
+        ExamHolidayAdapter adapter = new ExamHolidayAdapter(getContext(), hs);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml layout file.
+        ListView listView = rootView.findViewById(R.id.list_item_examholiday);
+
+        // Make the {@link ListView} use the {@link ScheduleAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link scheduleItem} in the list.
+        listView.setAdapter(adapter);
+        FloatingActionButton fab=getActivity().findViewById(R.id.fab);
+        fab.show();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("EAH","Clicked fab");
+            }
+        });
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
