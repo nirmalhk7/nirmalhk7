3a5975d306b8229c3fcadf619a765fbed1f190ac
nirmalhk7 <nirmalhk7@gmail.com>
2019-07-27 00:28:11 +0530

    Color

--- app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
         if (savedInstanceState == null) {
             navigationView.getMenu().performIdentifierAction(R.id.nav_dashboard, 0);
 
