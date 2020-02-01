2deff8591cdb764d131e3818d4dac88f163c99da
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-02 00:20:01 +0530

    Widget design completed
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- app/src/main/java/com/nirmalhk7/nirmalhk7/widget/Main.java
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/widget/Main.java
@@ -15,10 +15,8 @@ public class Main extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
