f0bbdf8b7326789f4db05daa6219d736dde9b421
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-02 14:45:36 +0530

    Widget commit
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- app/src/main/java/com/nirmalhk7/nirmalhk7/widget/Main.java
+++ app/src/main/java/com/nirmalhk7/nirmalhk7/widget/Main.java
@@ -3,9 +3,14 @@ package com.nirmalhk7.nirmalhk7.widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.nirmalhk7.nirmalhk7.Controllers.Converters;
+import com.nirmalhk7.nirmalhk7.DBGateway;
 import com.nirmalhk7.nirmalhk7.R;
+import com.nirmalhk7.nirmalhk7.model.ExamholidaysEntity;
+import com.nirmalhk7.nirmalhk7.model.TimetableEntity;
 
 /**
  * Implementation of App Widget functionality.
@@ -16,9 +21,48 @@ public class Main extends AppWidgetProvider {
                                 int appWidgetId) {
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
-
-
-        // Instruct the widget manager to update the widget
+        DBGateway dbGateway=DBGateway.getInstance(context);
+        int today_no= Converters.day_to_dayno(Converters.today_get("EEE"));
+        String time=Converters.today_get("hh:mm a");
+        TimetableEntity timetableEntity=dbGateway.getTTDao().getScheduleByDayAndTime(0,Converters.to_date(time,"hh:mm a"));
+        ExamholidaysEntity exam=dbGateway.getEHDAO().getNextEvent(
+                Converters.to_date(
+                        Converters.today_get("dd MM yyyy"),
+                        "dd MM yyyy"),
+                1
+        );
+        ExamholidaysEntity holiday=dbGateway.getEHDAO().getNextEvent(
+                Converters.to_date(
+                        Converters.today_get("dd MM yyyy"),
+                        "dd MM yyyy"),
+                2
+        );
+        try{
+            Log.d("CHECKX","Next "+timetableEntity.getTask());
+            views.setTextViewText(R.id.widgetClassName,timetableEntity.getTask());
+            views.setTextViewText(R.id.widgetClassTime,Converters.date_to(timetableEntity.getStartTime(),"hh:mm a"));
+            views.setTextViewText(R.id.widgetClassAttendance,"ATTENDANCE");
+        }catch (Exception e)
+        {
+            Log.e("WIDGET",e.getMessage());
+            views.setTextViewText(R.id.widgetClassName,"No More Classes!");
+        }
+        try{
+            Log.d("CHECKXE","Next "+exam.getmName());
+            views.setTextViewText(R.id.widgetExamName,exam.getmName());
+            views.setTextViewText(R.id.widgetExamDate,Converters.date_to(exam.getStart(),"MMM dd"));
+        }catch(NullPointerException e)
+        {
+            Log.e("WIDGET",e.getMessage());
+        }
+        try{
+            views.setTextViewText(R.id.widgetExamName,holiday.getmName());
+            views.setTextViewText(R.id.widgetExamDate,Converters.date_to(holiday.getStart(),"MMM dd"));
+        }catch(NullPointerException e)
+        {
+            Log.e("WIDGET",e.getMessage());
+        }
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
